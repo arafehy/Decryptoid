4372da2adb0c9e404115905d7b51d15619120b48
Yazan Arafeh <yazanarafeh24@gmail.com>
2019-12-15 21:41:17 -0800

    Simplified application flow

--- Homepage.php
+++ Homepage.php
@@ -3,7 +3,7 @@
     require_once 'helperFunctions.php';
 
     $conn = new mysqli($hn, $un, $pw, $db);
-    if($conn->connect_error) die(mysql_fatal_error());
+    if ($conn->connect_error) die(mysql_fatal_error());
 
     session_start();
     
@@ -19,22 +19,20 @@
     }
     
     if (isset($_SESSION['username'])) {
-
         ini_set('session.gc_maxlifetime', 60 * 60 * 24);
         $username = $_SESSION['username'];
 
         echo "Welcome back $username!<br>";
 
-        $query = "CREATE TABLE IF NOT EXISTS dataEntry(
+        $query = "CREATE TABLE IF NOT EXISTS inputs(
             entryTimeStamp TIMESTAMP NOT NULL,
             username VARCHAR(32) NOT NULL,
-            textInput TEXT,
-            fileInput BLOB,
-            convertedOutput BLOB
+            input TEXT,
+            cipherUsed VARCHAR(32)
         )";
 
         $result = $conn->query($query);
-        if(!$result) die (mysql_fatal_error());
+        if (!$result) die (mysql_fatal_error());
     }
 
     echo <<<_END
@@ -77,46 +75,23 @@ _END;
 
     if (isset($_POST['submitTextbox'])) {   // Get input from text box
         if (strlen($_POST['textArea']) != 0) {
-            
             $text = mysql_entities_fix_string($conn, $_POST['textArea']);
-            $text = preg_replace("/[^A-Za-z]/", '', $text);
-
-            encryptOrDecrypt($text, $conn, TRUE);
+            encryptOrDecrypt($text, $conn);
         }
     }
 
     else if (isset($_POST['submitFile'])) { // Get input from a text file
         if ($_FILES['selectedFile']['type'] == 'text/plain') {
-
-            $filename = $_FILES['selectedFile']['name'];
-            $text = mysql_entities_fix_string($conn, file_get_contents($filename));
-            $text = preg_replace("/[^A-Za-z]/", '', $text);
-
-            encryptOrDecrypt($text, $conn, FALSE);
+            $text = mysql_entities_fix_string($conn, file_get_contents($_FILES['selectedFile']['tmp_name']));
+            encryptOrDecrypt($text, $conn);
         }
         else {
             echo "The file you upload must be a text file.";
         }
     }
 
-    function encryptOrDecrypt($string, $conn, $bool) {
-        $output = "";
-        if ($bool) {
-            $textInput = $string;
-            $fileInput = "";
-        }
-        else {
-            $textInput ="";
-            $fileInput = $string;
-        }
-
-        $timestamp = date('Y-m-d G:i:s');
-
-        if (isset($_SESSION['username'])) {
-            $username = $_SESSION['username'];
-        }
-
-        if($_POST['ciphers'] == 'Simple Substitution') {
+    function encryptOrDecrypt($input, $conn) {
+        if ($_POST['ciphers'] == 'Simple Substitution') {
             require_once 'SimpleSubstitution.php';
 
             $cipher = new SimpleSubstitution();
@@ -126,69 +101,55 @@ _END;
                         "n"=>"v", "o"=>"d", "p"=>"z", "q"=>"x", "r"=>"k", "s"=>"i", "t"=>"o",
                         "u"=>"f", "v"=>"s", "w"=>"t", "x"=>"j", "y"=>"w", "z"=>"b"," "=> " ");
 
-            if($_POST['type'] == 'Encrypt') {
-                $output =  $cipher->encrypt($string, $pair);
-                echo $output;
+            if ($_POST['type'] == 'Encrypt') {
+                echo $cipher->encrypt($input, $pair);
             }
-            else if($_POST['type'] == 'Decrypt') {
-
-                $output = $cipher->Decrypt($string, $pair);
-                echo $output;
+            else if ($_POST['type'] == 'Decrypt') {
+                echo $cipher->decrypt($input, $pair);
             }
             if (isset($_SESSION['username'])) {
-                makeQuery($conn, $timestamp, $username, $textInput, $fileInput, $output);
+                $cipherUsed = 'Simple Substitution';
+                storeInput($conn, $input, $cipherUsed);
             }
-
         }
-        else if($_POST['ciphers']=='Double Transposition'){
+        else if ($_POST['ciphers'] == 'Double Transposition'){
             require_once 'DoubleTransposition.php';
 
             $cipher = new DoubleTransposition();
 
             if($_POST['type'] == 'Encrypt') {
-                $output = $cipher->encrypt("doggo","rocket", $string);
-                echo $output;
+                echo $cipher->encrypt("doggo","rocket", $input);
             }else if($_POST['type'] == 'Decrypt') {
 
-                $output = $cipher->decrypt("rocket", "doggo", $string);
-                echo $output;
+                echo $cipher->decrypt("rocket", "doggo", $input);
             }
-
             if (isset($_SESSION['username'])) {
-                makeQuery($conn, $timestamp, $username, $textInput, $fileInput, $output);
+                $cipherUsed = 'Double Transposition';
+                storeInput($conn, $input, $cipherUsed);
             }
-
-        }else if($_POST['ciphers'] =='RC4') {
+        }
+        else if($_POST['ciphers'] =='RC4') {
             require_once 'RC4.php';
 
             $cipher = new RC4();
 
-            if($_POST['type'] == 'Encrypt'){
-                $output = $cipher->rc4Cipher("secret", $string);
-                echo $output;
-            }
-            else if($_POST['type'] == 'Decrypt'){
-                $output = $cipher->rc4Cipher("secret", $string);
-                echo $output;
-            }
+            echo $cipher->rc4Cipher("vanilla", $input);
+
             if (isset($_SESSION['username'])) {
-                makeQuery($conn, $timestamp, $username, $textInput, $fileInput, $output);
+                $cipherUsed = 'RC4';
+                storeInput($conn, $input, $cipherUsed);
             }
         }
     }
 
-    function makeQuery($conn, $timestamp, $username, $text, $file, $output){
-        $stmt = $conn->prepare('INSERT INTO dataentry VALUES(?, ?, ?, ?, ?)');
-        $stmt->bind_param('sssss', $timestamp, $username, $text, $file, $output);
-        $username = mysql_entities_fix_string($conn, $username);
-        $text = mysql_entities_fix_string($conn, $text);
-        $file = mysql_entities_fix_string($conn, $file);
-        $output = mysql_entities_fix_string($conn, $output);
+    function storeInput($conn, $input, $cipherUsed){
+        $username = mysql_entities_fix_string($conn, $_SESSION['username']);
+        $stmt = $conn->prepare('INSERT INTO inputs VALUES(?,?,?,?)');
+        $stmt->bind_param('ssss', $timestamp, $username, $input, $cipherUsed);
+        $input = mysql_entities_fix_string($conn, $input);
+        $cipherUsed = mysql_entities_fix_string($conn, $cipherUsed);
         $stmt->execute();
-        $result = $stmt->get_result();
         $stmt->close();
-
-        if (!$result) die(mysql_fatal_error());
     }
 
     // Close the result and connection to remove from memory so that attackers can't access them
